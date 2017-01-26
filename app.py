e4be9c732f2d43d9afa7bb031d8abc9324938f98
Thomas Legrand <thomas.legrand1@gmail.com>
2017-01-26 11:09:34 -0800

    basis for 404 error handling

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import numpy as np
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, abort
 from keras.models import load_model
 import tensorflow as tf
-from constants import *
+import constants
 
-model = load_model(MLP_MODEL_FILENAME)
+model = load_model(constants.CNN_MODEL_FILENAME)
 
 # get the default graph for future reference.
 # see https://github.com/fchollet/keras/issues/2397#issuecomment-254919212
@@ -17,17 +17,25 @@ app = Flask(__name__)
 @app.route('/mnist/classify', methods=['POST'])
 def make_predict():
     # error checking
+    if False:
+        abort(404)
+
     data = request.get_json(force=True)
     # convert data to numpy array
-    # data to predict
-    predict_request = np.array(data)
+    predict_request = np.array(data).reshape(1, constants.FLAT_IMAGE_LENGTH)
+
     global graph
     with graph.as_default():
-        preds = model.predict_classes(predict_request.reshape(1, 784), verbose=0)
-    output = {"ddmlp": str(preds[0])}
+        preds = model.predict_classes(predict_request, verbose=0)
+
     # jsonify is the safe way to generate a JSON file to return
     # see http://flask.pocoo.org/docs/0.10/security/#json-security
-    return jsonify(output)
+    return jsonify({"classification": str(preds[0])})
+
+
+@app.errorhandler(404)
+def page_not_found():
+    return render_template('404.html', planet_ascii_art=constants.PLANET_ASCII_ART), 404
 
 
 if __name__ == "__main__":
